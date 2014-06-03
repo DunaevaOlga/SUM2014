22b11f328d475024d21c2577ab14bbf1bf20c065
unknown <SPR05@R111-26.(none)>
2014-06-03 18:38:55 +0400

    ...

--- T01/T01.C
+++ T01/T01.C
@@ -1,7 +1,11 @@
 #include <windows.h>
+#include <math.h>
+#define SQR(X) ((X) * (X))
 #define WND_CLASS_NAME "My window class"
 
 LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam );
+
+
 
 INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT ShowCmd)
 {
@@ -49,6 +53,41 @@ INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine,
 }/*End of 'WinMain' function*/
 
 
+
+void DrawEye (HDC hDC, int Xm, int Ym, int Xc, int Yc, int W, int H, int R)
+{
+  int xx, yy;
+  float 
+    len, si, co,
+    ratio = (float)W / H;
+
+  len = sqrt(SQR(Xc - Xm) + SQR((Yc - Ym) * ratio));
+
+  if ( len == 0)
+    si = 0, co = 0;
+  else
+    co = (Xm - Xc) / len, si = (Ym - Yc) * ratio / len;
+ 
+  SelectObject(hDC, GetStockObject(DC_PEN));
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+
+ 
+  if (len > W / 2 - R)
+    len = W / 2 - R;
+  xx = Xc + co * len;
+  yy = Yc + si * len / ratio;
+
+  SetDCBrushColor(hDC, RGB(255, 255, 255));
+  Ellipse(hDC, xx - R, yy - R / ratio, xx + R, yy + R / ratio);
+
+  SetDCPenColor(hDC, RGB(255, 255, 255));
+  SetDCBrushColor(hDC, RGB(0, 0, 0));
+
+  Ellipse(hDC, Xc - W / 2, Yc - H / 2, Xc + W / 2, Yc + H / 2);
+
+}                        
+
+
 float ro( void )
 {
   return (float)rand() / RAND_MAX;
@@ -56,31 +95,34 @@ float ro( void )
 
 LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam )
 {
-  HDC hDC;
+  HDC hDC, hMemDC, hMemDC1;
   PAINTSTRUCT pc;
   RECT rc;
+  POINT pt;
 
   int x0, y0, x1, y1, i;
 
   switch(Msg)
   {
+  case WM_CREATE:
+    SetTimer(hWnd, 30, 50, NULL);
+    return 0;
+ 
   case WM_PAINT:
     hDC = BeginPaint(hWnd, &pc);
     GetClientRect(hWnd, &rc);
+
+    
    
     srand(30);
     SelectObject(hDC, GetStockObject(NULL_PEN));
-    SelectObject(hDC, GetStockObject(DC_BRUSH));
-       
-    for (i = 0; i < 10; i++)
-    { 
-      SetDCBrushColor(hDC, RGB(rand() % 256, rand() % 256, rand() % 256)); 
-      x0 = ro() * rc.right;
-      x1 = ro() * rc.right;
-      y0 = ro() * rc.bottom;
-      y1 = ro() * rc.bottom;
-      Ellipse(hDC, x0, y0, x1, y1);
-    }
+   /* SelectObject(hDC, GetStockObject(DC_BRUSH)); */
+
+    GetCursorPos(&pt);
+    ScreenToClient(hWnd, &pt);
+     
+    DrawEye(hDC, pt.x, pt.y, ro() * rc.right, ro() * rc.bottom, rand() % 20, rand() % 20, rand() % 20);
+  
     EndPaint(hWnd, &pc);
     return 0;
 
@@ -91,6 +133,7 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 
   case WM_DESTROY:
     PostQuitMessage(0);
+    KillTimer(hWnd, 30);
     return 0;
   }
   return DefWindowProc(hWnd, Msg, wParam, lParam);
